57602922c5de19d2ea57534d9760a94d94feb761
Pearlwhitehat2021 <78302254+Pearlwhitehat2021@users.noreply.github.com>
2021-08-03 12:20:30 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,79 +1,80 @@
-var bg,bgImg;
-var sleep,sleeping;
-var brush,brushing;
-var gym,gymimg;
-var eat,eating;
-var drink,drinkimg;
-var move,moveimg;
-var astro,astroImg;
-var bath;
+var bg,sleep, brush, gym, eat, bath, move;
+var astronaut;
 
 function preload(){
-  bgImg= loadImage("iss.png");
-  sleep= loadAnimation("sleep.png");
-  brush= loadAnimation("brush.png");
-  gym= loadAnimation("gym1.png","gym2.png");
-  eat = loadAnimation("eat1.png","eat2.png");
-  drink= loadAnimation("drink1.png","drink2.png");
-  move = loadAnimation("move.png","move1.png");
-  bath = loadAnimation("bath1.png","bath2.png");
+  bg= loadImage("iss.png");
+  sleep = loadAnimation("sleep.png");
+  brush = loadAnimation("brush.png");
+  gym = loadAnimation("gym1.png","gym1.png","gym2.png","gym2.png");
+  eat = loadAnimation("eat1.png","eat1.png","eat1.png","eat2.png","eat2.png","eat2.png");
+  bath = loadAnimation("bath1.png","bath1.png","bath1.png","bath2.png","bath2.png","bath2.png");
+ move = loadAnimation("move.png","move.png","move1.png","move1.png");
 }
 
-
 function setup() {
-  createCanvas(800,400);
-  /*bg = createSprite(400, 200, 50, 50);
-  bg.addImage(bgImg);
-  bg.scale=0.2;*/
-
-  astro = createSprite(300,230);
-  //astro = createEdgeSprites();
-  astro.addAnimation("sleeping",sleep);
-  astro.scale=0.1;
-
-
+  createCanvas(600, 500);
+  
+  astronaut = createSprite(300,230);
+  astronaut.addAnimation("sleeping", sleep);
+  astronaut.scale = 0.1;
   
 }
 
 function draw() {
- background(bgImg);  
- drawSprites();
- edges=createEdgeSprites();
-  astro.bounceOff(edges);
-  if (keyIsDown(UP_ARROW)){
-    astro.addAnimation("brushing",brush)
-    astro.changeAnimation("brushing");
-    astro.y = 350;
-    astro.velocityX = 0;
-    astro.velocityY = 0;
+  background(bg);
+  drawSprites();
+
+  textSize(20);
+  fill("white")
+  text("Instructions:",20, 35);
+  textSize(15);
+  text("Up Arrow = Brushing",20, 55);
+  text("Down Arrow = Gymming",20, 70);
+  text("Left Arrow = Eating",20, 85);
+  text("Right Arrow = Bathing",20, 100);
+  text("m key = Moving",20, 115);
+  
+  edges=createEdgeSprites();
+  astronaut.bounceOff(edges);
+  
+  if(keyDown("UP_ARROW")){
+    astronaut.addAnimation("brushing", brush);
+    astronaut.changeAnimation("brushing");
+    astronaut.y = 350;
+    astronaut.velocityX = 0;
+    astronaut.velocityY = 0;
   }
-  if (keyIsDown(DOWN_ARROW)){
-    astro.addAnimation("gyming",gym);
-    astro.changeAnimation("gyming");
-    astro.y = 370;
-    astro.velocityX = 0;
-    astro.velocityY = 0;
+  
+  if(keyDown("DOWN_ARROW")){
+    astronaut.addAnimation("gymming", gym);
+    astronaut.changeAnimation("gymming");
+    astronaut.y = 350;
+    astronaut.velocityX = 0;
+    astronaut.velocityY = 0;
   }
-  if (keyIsDown(RIGHT_ARROW)){
-    astro.addAnimation("eating",eat);
-    astro.addAnimation("eating");
-    astro.x = 300;
-    astro.velocityX = 0;
-    astro.velocityY = 0;
+  
+  if(keyDown("LEFT_ARROW")){
+    astronaut.addAnimation("eating", eat);
+    astronaut.changeAnimation("eating");
+    astronaut.x = 150;
+    astronaut.y = 350;
+    astronaut.velocityX = 0.5;
+    astronaut.velocityY = 0.5;
   }
-  if (keyIsDown(LEFT_ARROW)){
-    astro.addAnimation("bathing",bath);
-    astro.changeAnimation("bathing");
-    astro.x = 150;
-    astro.velocityX = 0;
-    astro.velocityY = 0;
-  }
-  if(keyIsDown("m")){
-    astro.addAnimation("moving",move);
-    astro.changeAnimation("moving");
-    astro.velocityX = 2;
-    astro.velocityY = 2;
+  
+  if(keyDown("RIGHT_ARROW")){
+    astronaut.addAnimation("bathing", bath);
+    astronaut.changeAnimation("bathing");
+    astronaut.x = 300;
+    astronaut.velocityX = 0;
+    astronaut.velocityY = 0;
   }
 
-  
+  if(keyDown("m")){
+    astronaut.addAnimation("moving", move);
+    astronaut.changeAnimation("moving");
+    astronaut.velocityX = 1;
+    astronaut.velocityY = 1;
+  }
+
 }
